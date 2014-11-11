e844012b3f302c47bf9843092cc6253e98d90c5d
JumuFENG <zhcbfly@qq.com>
2014-11-11 23:16:56 +0800

    Add lib of memory dynamic operations

---
+++ libMemDO/src/MemDO.cpp
@@ -0,0 +1,27 @@
+#include "MemDO.h"
+#include "helper/helper.h"
+
+int mdo_modify_memory(const tstring& strProcName, unsigned long offset, 
+    byte* content, size_t nsize
+    )
+{
+    DWORD dwPid = helper::process::get_process_by_name(strProcName);
+    unsigned long procStart = helper::process::get_process_base_address(dwPid);
+    unsigned long realAddress = procStart + offset;
+    HANDLE hTargetProc = OpenProcess(PROCESS_ALL_ACCESS|PROCESS_TERMINATE|
+        PROCESS_VM_OPERATION|PROCESS_VM_READ|
+        PROCESS_VM_WRITE, FALSE, dwPid);
+    if (hTargetProc == NULL)
+    {
+        return 1;
+    }
+    SIZE_T szWritten;
+    if ( !WriteProcessMemory(hTargetProc, 
+        (LPVOID)realAddress, content, nsize, &szWritten))
+    {
+        CloseHandle(hTargetProc);
+        return GetLastError();
+    }
+    CloseHandle(hTargetProc);
+    return 0;
+}
---
+++ libMemDO/src/MemDO.h
@@ -0,0 +1,19 @@
+#ifndef MEM_DYNAMIC_OPERATE_HEADER_
+#define MEM_DYNAMIC_OPERATE_HEADER_
+
+#include "helper/tstring.h"
+#include <Windows.h>
+
+/** 
+ * @param strProcName [in] 进程名
+ * @param offset [in] 要替换的内存地址相对于进程起始地址的偏移量
+ * @param content [in] 要替换的内容
+ * @param nsize [in] 要替换的字节数
+ * @ret 成功返回0
+ * 本接口修改进程的内存值，若替换的是进程中所执行的代码必须由调用者确保
+ * 替换前后相应位置的内容不会影响前后代码段的正常执行。Be Carefully!
+ */
+int mdo_modify_memory(const tstring& strProcName, unsigned long offset,
+    byte* content, size_t nsize);
+
+#endif //MEM_DYNAMIC_OPERATE_HEADER_
